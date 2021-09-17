d547b8f866e97ae28c9ce3a3954cf72daa5ed531
LMFT <lucasferrini1607@gmail.com>
2021-09-17 17:36:47 -0300

    Implementado metodo GenerarBiblioteca, que carga un listado con todo el software y juegos disponibles en el atributo privado biblioteca

--- FormCiber/Computadora/Computadora.cs
+++ FormCiber/Computadora/Computadora.cs
@@ -8,25 +8,40 @@ namespace Dispositivos.Computadora
     {
         static Dictionary<string, string> biblioteca;
 
+        public static void GenerarBiblioteca()
+        {
+            string[] software = {"ICQ", "Ares", "Office", "Messenger", "Winamp"};
+            string[] juegos = {"Counter Strike", "Diablo 2", "Warcraft 3", "Mu Online", "Lineage 2", "Age of Empires 2", "GTA Vice City"};
+            
+            foreach(string item in software)
+            {
+                biblioteca.Add(item, "software");
+            }
+            foreach(string item in juegos)
+            {
+                biblioteca.Add(item, "juego");
+            }
+        }
+
         
     }
 
     public class Computadora : Dispositivo
     {
-        List<string> softwareInstalado;
+        Dictionary<string, string> softwareInstalado;
         List<string> perifericos;
         Dictionary<string, string> especificaciones;
 
         
-        public Computadora(Dictionary<string, string> especificaciones, List<string> perifericos, List<string> software, string id
-            , Estado estado, double costoFraccion): base(id, estado, costoFraccion)
+        public Computadora(Dictionary<string, string> especificaciones, List<string> perifericos, Dictionary<string, string> software,
+            string id, Estado estado, double costoFraccion): base(id, estado, costoFraccion)
         {
             this.SoftwareInstalado = software;
             this.Especificaciones = especificaciones;
             this.Perifericos = perifericos;
         }
 
-        public List<string> SoftwareInstalado { get => softwareInstalado; set => softwareInstalado = value; }
+        public Dictionary<string, string> SoftwareInstalado { get => softwareInstalado; set => softwareInstalado = value; }
         public List<string> Perifericos { get => perifericos; set => perifericos = value; }
         public Dictionary<string, string> Especificaciones { get => especificaciones; set => especificaciones = value; }
     }
